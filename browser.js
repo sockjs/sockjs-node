3b017913b7054b421c6722e3677bbeb1cba28cd8
James Halliday <mail@substack.net>
2012-06-20 20:07:36 -0700

    streamable browser variant for browserify

---
+++ browser.js
@@ -0,0 +1,39 @@
+var Stream = require('stream');
+var sockjs = require('sockjs-client');
+
+module.exports = function (uri, cb) {
+    if (/^\/\/[^\/]+\//.test(uri)) {
+        uri = window.location.protocol + uri;
+    }
+    else if (!/^https?:\/\//.test(uri)) {
+        uri = window.location.protocol + '//'
+            + window.location.host
+            + (/^\//.test(uri) ? uri : '/' + uri)
+        ;
+    }
+    
+    var stream = new Stream;
+    stream.readable = true;
+    
+    var sock = sockjs(uri);
+    stream.sock = sock;
+    
+    stream.write = function (msg) {
+        sock.send(msg);
+    };
+    
+    sock.onopen = function () {
+        stream.writable = true;
+        if (typeof cb === 'function') cb();
+    };
+    sock.onmessage = function (e) {
+        stream.emit('data', e.data);
+    };
+    sock.onclose = function () {
+        stream.emit('end');
+        stream.writable = false;
+        stream.readable = false;
+    };
+    
+    return stream;
+};
