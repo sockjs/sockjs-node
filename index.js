ad55439080f1a9944ffddbaae404a94fb131f393
Bryce Kahle <bkahle@gmail.com>
2018-08-12 11:57:09 -0400

    Move sockjs content to index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-module.exports = require('./lib/sockjs');
+'use strict';
+
+const Server = require('./lib/server');
+
+module.exports.createServer = function createServer(options) {
+  return new Server(options);
+};
+
+module.exports.listen = function listen(http_server, options) {
+  const srv = exports.createServer(options);
+  if (http_server) {
+    srv.installHandlers(http_server);
+  }
+  return srv;
+};
