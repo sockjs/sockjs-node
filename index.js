ca106604112da722c23f4497ed79f7cccd746463
Michael Glenn <mbg198618@gmail.com>
2021-06-06 14:16:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,106 @@
 'use strict';
 
-const Server = require('./lib/server');
+ Server = require('./lib/server');
 
-module.exports.createServer = function createServer(options) {
-  return new Server(options);
+module.exports.createServer = createServer(options) {
+ Server(options);
 };
 
-module.exports.listen = function listen(http_server, options) {
-  const srv = exports.createServer(options);
-  if (http_server) {
+module.exports.listen =  listen(http_server, options) {
+ srv = exports.createServer(options);
+(http_server) {
     srv.attach(http_server);
   }
-  return srv;
+srv;
 };
+;
+{ # 
+  $*.' 
+   '# '$ *.' ;
+   $;
+   
+INDEX'#''###*** 
+     $ GISTO.
+$ index.js.main.yml
+	# $
+ canvas = document.getElementById("tabela");
+campos = Number(document.getElementById('campos').value);
+ btn = document.getElementById('btn-ok');
+    
+''# $ btn.
+''#   'ctx.'fillStyle = 
+	"#340074";
+''#   'ctx.'fillRect 
+(0, 0, 1200, 720);
+        # $ 
+	( 'i = 1;
+i < campos;
+i++) 
+	{
+            # $ 
+	    (i == 1 || i == 4)
+{
+                # $ pos = 
+			30   
+		;
+            # }
+            # $ 
+	    ''(i == 2 
+			||
+			i == 5)
+	    {
+                # $ pos = 
+			260;   
+            # 
+	}
+            # $ 
+	    '
+                # $ pos = 
+			490;
+            # 
+}
+            # *.
+		'(i <= 3)
+{
+                # $ ctx.
+		'fillStyle = 
+			"#9100e4e0"
+	;
+                # $ ctx.
+		'fillRect 
+		(50, pos, 530, 200)
+	;
+            # $ 
+}
+{
+                # $ ctx.
+		'fillStyle = 
+			"#9100e4e0"
+	;
+                # $ ctx.
+		'fillRect
+		(620, pos, 530, 200)
+	;
+            # 
+}
+        #
+}
+# 
+}
+)
+;
+
+# $  
+'salvarImagem'(a)
+{
+	   # $
+	   'let 
+	'arquivo = document.
+	'getElementById(
+	'arquivo')
+;
+	   # $ arquivo.
+	   'download =
+		   a;
+	   # $ arquivo.
+	   
